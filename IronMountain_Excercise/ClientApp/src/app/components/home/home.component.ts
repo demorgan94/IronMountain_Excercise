bbccb0b5859b8e2bf328007d92d8e442be40bcd4
David Morgan <demorgan94@hotmail.com>
2020-03-02 12:21:55 -0700

    gitflow-feature-stash: ProcessImages

---
+++ IronMountain_Excercise/ClientApp/src/app/components/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { UploadImagesService } from 'src/app/services/upload-images.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+  fileData: File[];
+  message: string = "Choose Image(s)";
+
+  constructor(private _uploadImagesService: UploadImagesService) { }
+
+  uploadImages(images: any) {
+    if(images.length === 0) {
+      return;
+    }
+
+    this.fileData = images.target.files;
+
+    const formData = new FormData();
+
+    for (let i = 0; i < this.fileData.length; i++) {
+      formData.append(this.fileData[i].name, this.fileData[i]);
+    }
+
+    this._uploadImagesService.uploadImages(formData).subscribe(res => {
+      this.message = "Image(s) Uploaded Successfully";
+    }, err => {
+      this.message = "Error Uploading Image(s)";
+    });
+  }
+}
